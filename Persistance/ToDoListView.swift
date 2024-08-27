e9bed62279d204f490a3675aef79e48c3dbe09f5
Liam Seymour <spaceturatle@gmail.com>
2024-08-27 09:43:26 -0500

    dsadw

--- Persistance/ToDoListView.swift
+++ Persistance/ToDoListView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ToDoListView: View {
     var body: some View {
-        VStack {
-            Text("To Do List")
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .ignoresSafeArea()
+                .foregroundColor(.orange)
+            RoundedRectangle(cornerRadius: 45)
+                .foregroundColor(.white)
+                .frame(width: 365, height: 825)
+                .offset(y: -12)
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.orange)
+                        .shadow(color: .orange, radius: 10)
+                    Text("To Do List")
+                }
+                .offset(y: 30)
+                Spacer()
+                
+            }
         }
     }
 }
