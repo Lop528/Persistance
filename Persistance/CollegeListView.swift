afa45e63cc32602dc727a9ae59ef38dfe035dff2
Liam Seymour <spaceturatle@gmail.com>
2024-08-27 09:31:15 -0500

    added views and titles

---
+++ Persistance/CollegeListView.swift
@@ -0,0 +1,20 @@
+//
+//  CollegeListView.swift
+//  Persistance
+//
+//  Created by Liam K. Seymour on 8/27/24.
+//
+
+import SwiftUI
+
+struct CollegeListView: View {
+    var body: some View {
+        VStack {
+            Text("Colleges")
+        }
+    }
+}
+
+#Preview {
+    CollegeListView()
+}
---
+++ Persistance/ToDoListView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Persistance
+//
+//  Created by Liam K. Seymour on 8/27/24.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        VStack {
+            Text("To Do List")
+        }
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
