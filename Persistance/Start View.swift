78fafdd73eb185821de574b8f7a185e2832591e0
Liam Seymour <spaceturatle@gmail.com>
2024-09-05 08:48:57 -0500

    new biew and othe rstuff

---
+++ Persistance/Start View.swift
@@ -0,0 +1,18 @@
+//
+//  Start View.swift
+//  Persistance
+//
+//  Created by Liam K. Seymour on 9/5/24.
+//
+
+import SwiftUI
+
+struct Start_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Start_View()
+}
